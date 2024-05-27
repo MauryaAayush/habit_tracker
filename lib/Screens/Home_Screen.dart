e804fd832e5f04dc989a8fa69f1daa15400cd8aa
Admin <maayush235@gmail.com>
2024-05-28 00:59:40 +0530

    Radhey Radhey

---
+++ lib/Screens/Home_Screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import '../Themes/Theme_Provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        child: Center(
+          child: CupertinoSwitch(
+            value: Provider.of<ThemeProvider>(context).isDarkMode,
+            onChanged: (value) {
+              Provider.of<ThemeProvider>(context,listen: false).toggleTheme();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
