f76d012981970d3a06f99678b0faace0a623b5bd
Admin <maayush235@gmail.com>
2024-05-28 16:27:10 +0530

    Radhey Radhey

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/Components/My_Drawer.dart';
 
+final TextEditingController textEditingController = TextEditingController();
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  // create  new habit
   @override
   Widget build(BuildContext context) {
+    void createNewHabit() {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: TextField(
+            controller: textEditingController,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(),
       drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+      ),
     );
   }
 }
