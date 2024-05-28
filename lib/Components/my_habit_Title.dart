323d91af3388f74e21ef6bae148e0a3aa7f7715b
Admin <maayush235@gmail.com>
2024-05-28 19:08:45 +0530

    Radhey Radhey

--- lib/Components/my_habit_Title.dart
+++ lib/Components/my_habit_Title.dart
@@ -13,11 +13,12 @@ class MyHabitTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(12),
-      margin: const EdgeInsets.all(12),
+      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
       decoration: BoxDecoration(
         color: isCompleted
             ? Colors.green
             : Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.circular(8)
       ),
       child: Text(text),
     );
