978dbf64d92cf6e58552985990ba2b7ff17cfd31
Admin <maayush235@gmail.com>
2024-05-28 19:50:20 +0530

    Radhey Radhey

--- lib/Components/my_habit_Title.dart
+++ lib/Components/my_habit_Title.dart
@@ -14,20 +14,25 @@ class MyHabitTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(12),
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-      decoration: BoxDecoration(
-          color: isCompleted
-              ? Colors.green
-              : Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(8)),
-      child: ListTile(
-        title: Text(text),
-        leading: Checkbox(
-          activeColor: Colors.green,
-            value: isCompleted,
-            onChanged: onChanged,
+    return GestureDetector(
+      onTap: () {
+
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+        decoration: BoxDecoration(
+            color: isCompleted
+                ? Colors.green
+                : Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(8)),
+        child: ListTile(
+          title: Text(text),
+          leading: Checkbox(
+            activeColor: Colors.green,
+              value: isCompleted,
+              onChanged: onChanged,
+          ),
         ),
       ),
     );
