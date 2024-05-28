fa6c248aa4d8cd1353021ccaa536f7adb9e8ea1b
Admin <maayush235@gmail.com>
2024-05-28 19:14:53 +0530

    Radhey Radhey

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -100,10 +100,8 @@ class _HomeScreenState extends State<HomeScreen> {
         bool isCompletedToday = isHabitCompletedToday(habit.completeddays);
 
         // return habit tile UI
-          return MyHabitTitle(text: habit.name, isCompleted: isCompletedToday);
-
+        return MyHabitTitle(text: habit.name, isCompleted: isCompletedToday,onChanged: (value) => checkHabitOnOff(value),);
       },
     );
   }
 }
-
