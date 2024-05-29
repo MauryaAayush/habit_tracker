e0ae9560d3694a7b2ea7d44fc9e163d30ea15e95
Admin <maayush235@gmail.com>
2024-05-29 10:36:59 +0530

    Radhey Radhey

--- lib/Components/my_habit_Title.dart
+++ lib/Components/my_habit_Title.dart
@@ -1,40 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MyHabitTitle extends StatelessWidget {
   final bool isCompleted;
   final String text;
   final void Function(bool?)? onChanged;
+  final void Function(BuildContext)? editHabit;
 
   const MyHabitTitle({
     super.key,
     required this.text,
     required this.isCompleted,
     required this.onChanged,
+    required this.editHabit,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (onChanged != null) {
-          // toggle completion status
-          onChanged!(!isCompleted);
-        }
-      },
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-        decoration: BoxDecoration(
-            color: isCompleted
-                ? Colors.green
-                : Theme.of(context).colorScheme.secondary,
-            borderRadius: BorderRadius.circular(8)),
-        child: ListTile(
-          title: Text(text),
-          leading: Checkbox(
-            activeColor: Colors.green,
-            value: isCompleted,
-            onChanged: onChanged,
+    return Slidable(
+      endActionPane: ActionPane(motion: const StretchMotion(), children: [
+        //   edit option
+        SlidableAction(
+          onPressed: editHabit,
+          backgroundColor: Colors.green.shade800,
+          icon: Icons.settings,
+          borderRadius: BorderRadius.circular(8),
+        ),
+
+        //   delete option
+      ]),
+      child: GestureDetector(
+        onTap: () {
+          if (onChanged != null) {
+            // toggle completion status
+            onChanged!(!isCompleted);
+          }
+        },
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+          decoration: BoxDecoration(
+              color: isCompleted
+                  ? Colors.green
+                  : Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(8)),
+          child: ListTile(
+            title: Text(text),
+            leading: Checkbox(
+              activeColor: Colors.green,
+              value: isCompleted,
+              onChanged: onChanged,
+            ),
           ),
         ),
       ),
